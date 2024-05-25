3eb0607b32d042af98994ee941a40d2affd125a2
AndreaQuin12 <techfn530@gmail.com>
2024-05-25 17:40:24 -0600

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  roots: ['./frontend/src'],
+  roots: ['frontend/src'],
   testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
   transform: {
     '^.+\\.js$': 'babel-jest',
